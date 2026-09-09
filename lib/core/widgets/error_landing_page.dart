d7836eadbba2ccadc18912102f94e447ea5e3405
Nottackelgamer <minecraftidaryan72@gmail.com>
2026-09-09 22:48:05 +0530

    fix(core): restore AppColors import in error_landing_page.dart

--- lib/core/widgets/error_landing_page.dart
+++ lib/core/widgets/error_landing_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:go_router/go_router.dart';
+import '../../app/theme/app_colors.dart';
 import '../../app/theme/app_text_styles.dart';
 import '../../shared/widgets/app_logo.dart';
 
