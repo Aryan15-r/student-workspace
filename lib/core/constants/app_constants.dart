841be6afc557006ed5342611926167fad91afab3
Nottackelgamer <minecraftidaryan72@gmail.com>
2026-09-11 00:34:18 +0530

    chore(release): bump version to v1.0.34

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -8,7 +8,7 @@ class AppConstants {
   static const String appName = 'StudySpace';
   static const String appTagline =
       'One workspace. Less switching. More learning.';
-  static const String appVersion = '1.0.33';
+  static const String appVersion = '1.0.34';
 
   /// Google Gemini REST API endpoint
   static const String geminiBaseUrl =
